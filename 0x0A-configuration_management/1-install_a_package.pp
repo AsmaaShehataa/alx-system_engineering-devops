81b0ebb8479e4f93363374c3f4f131c0a99eb4bc
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-12-23 00:54:39 +0200

    installflask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Create a puppet manifest file to install flask package
 package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
-    }
+  ensure   => 'present',
+  provider => 'pip3',
+  install_options => ['flask==2.1.0'],
+}

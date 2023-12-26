093645d02701e1008ade07e2ccad0a94200fc002
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-12-27 00:47:49 +0200

    Puppet config for task 2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # Client SSH config 
-file_line { 'Turn off passwod auth':
+file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line    => '  PasswordAuthentication no',
 }
-file_line { 'Declare identity file'
+file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line  => '  IdentityFile ~/.ssh/school'

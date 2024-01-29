6e05eeca82e731c8c84631d7c86ac8c9e5343ba9
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-01-29 19:11:46 +0200

    code fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,22 @@
-
 #!/usr/bin/python3
 """Given Employee information, return their todo list progress."""
 
 if __name__ == "__main__":
     import requests
     import sys
-    import urllib3
-    urllib3.disable_warnings(urllib3.exceptions.NotOpenSSLWarning)
 
     NUMBER_OF_DONE_TASKS = 0
     TASK_TITLE = []
     USER_ID = sys.argv[1]
 
-    user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/{USER_ID}').json()
-    USERNAME = user_info.get("username")
+    user_info = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{USER_ID}'
+    ).json()
+    _name = user_info.get('name')
 
-    _request = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={USER_ID}').json()
+    _request = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={USER_ID}'
+    ).json()
 
     for _task in _request:
         if _task.get('completed') is True:
@@ -25,7 +26,7 @@ if __name__ == "__main__":
     TOTAL_NUMBER_OF_TASKS = len(_request)
 
     print('Employee {} is done with tasks({}/{}):'.
-          format(USERNAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+          format(_name, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
 
     for title in TASK_TITLE:
         print('\t {}'.format(title))

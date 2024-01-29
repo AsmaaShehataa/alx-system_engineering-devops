b408a857b087769d6ec7b3a88d8061d78bdf9261
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-01-29 19:01:46 +0200

    code fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,31 @@
 
 #!/usr/bin/python3
-"""Python script that, using this REST API, for a given employee ID,"""
+"""Given Employee information, return their todo list progress."""
 
-import re
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
+    import urllib3
+    urllib3.disable_warnings(urllib3.exceptions.NotOpenSSLWarning)
 
-REST_API = "https://jsonplaceholder.typicode.com"
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
+    USER_ID = sys.argv[1]
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
-                )
-            )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+    user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/{USER_ID}').json()
+    USERNAME = user_info.get("username")
+
+    _request = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={USER_ID}').json()
+
+    for _task in _request:
+        if _task.get('completed') is True:
+            TASK_TITLE.append(_task.get('title'))
+            NUMBER_OF_DONE_TASKS += 1
+
+    TOTAL_NUMBER_OF_TASKS = len(_request)
+
+    print('Employee {} is done with tasks({}/{}):'.
+          format(USERNAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    for title in TASK_TITLE:
+        print('\t {}'.format(title))

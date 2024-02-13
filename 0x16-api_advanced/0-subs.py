02ae002c05427062814ec7eae7d28e9b0c2cc155
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-02-13 17:00:11 +0200

    returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,4 +14,8 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
+    data = response.json()['data']
+    page_list = data['children']
+    page_data = page_list[0]['data']
+
     return response.json().get('data').get('subscribers')

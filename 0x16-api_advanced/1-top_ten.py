008849ed9d5db640017acc80b2ccb1e5dde44565
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-02-13 17:39:53 +0200

    retrieve titles of posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,18 @@
-
 #!/usr/bin/python3
-"""prints the titles of the first 10 hot posts listed for a given subreddit"""
+""" retrieve titles of posts """
+
+import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed for a given subreddit"""
-    import requests
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    """ function to retrieve post titles 10 in number """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-agent": "nadduli daniel"}
 
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    posts = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
-        print(None)
+    if posts.status_code == 200:
+        for post in posts.json()['data']['children']:
+            print(post['data']['title'])
     else:
-        for post in response.json().get('data').get('children'):
-            print(post.get('data').get('title'))
+        print(None)

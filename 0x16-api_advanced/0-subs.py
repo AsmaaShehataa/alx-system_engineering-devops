00720c81fbce7af964c5fc79ec9330199125cd0c
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-02-13 16:56:01 +0200

    returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Query Reddit API and returns the number of subscribers """
+
+import requests
+def number_of_subscribers(subreddit):
+    """ Query Reddit API and returns the number of subscribers """
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    return response.json().get('data').get('subscribers')

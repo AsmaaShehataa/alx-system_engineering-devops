87dc60aca74bba33566f3e9b1ee94aa8f2249665
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-02-13 17:24:31 +0200

    returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit API
-"""
 
-from requests import get
+""" given Reddit API and returns the number of subscribers"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    """ Reddit API and returns the number of subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
+    return response.json().get('data').get('subscribers')
